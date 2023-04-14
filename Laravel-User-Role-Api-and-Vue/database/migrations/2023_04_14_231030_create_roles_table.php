6bd9bfbca080eaa022edad826f6c36452f5df147
GBOTOUNOU <haroldgbotounou1@gmail.com>
2023-04-15 00:21:01 +0100

    Migration and DB setup

---
+++ Laravel-User-Role-Api-and-Vue/database/migrations/2023_04_14_231030_create_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 255);
+            $table->integer('niveau')->default(1); // définir la valeur par défaut à 1
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+};
--- Laravel-User-Role-Api-and-Vue/database/migrations/2023_04_14_231116_create_users_table.php
+++ Laravel-User-Role-Api-and-Vue/database/migrations/2023_04_14_231116_create_users_table.php
@@ -15,11 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nom', 255);
+            $table->string('prenom', 255);
+            $table->string('sexe', 10);
+            $table->date('date_naissance');
+            $table->string('telephone', 20);
+            $table->string('nom_utilisateur', 255)->unique();
+            $table->string('mot_de_passe', 255);
+            $table->string('photo_de_profile', 255);
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
         });
     }
