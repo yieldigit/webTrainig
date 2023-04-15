5fedfb2953f4061a2ab759de30bc7f0da6720c80
GBOTOUNOU <haroldgbotounou1@gmail.com>
2023-04-15 10:18:02 +0100

    Setting up routes in the routes/api.php

--- Laravel-User-Role-Api-and-Vue/routes/api.php
+++ Laravel-User-Role-Api-and-Vue/routes/api.php
@@ -14,6 +14,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/* JWt route */
+
+//Route for authentification
+Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+
+//Route for logout or refresh tokens
+Route::middleware('jwt.auth')->group(function () {
+    Route::post('/logout', 'AuthController@logout');
+    Route::post('/refresh', 'AuthController@refresh');
+});
+
+// Routes for CRUD operations on roles
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('role', 'RoleController@index');
+    Route::get('role/{id}', 'RoleController@show');
+    Route::post('role', 'RoleController@store');
+    Route::put('role/{id}', 'RoleController@update');
+    Route::delete('role/{id}', 'RoleController@destroy');
+    Route::delete('/role/{id}', 'RoleController@destroy')->middleware('auth.jwt', 'check.role:3');
+});
+
+// Routes for CRUD operations on users
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('user', 'UserController@index');
+    Route::get('user/{id}', 'UserController@show');
+    Route::post('user', 'UserController@store');
+    Route::put('user/{id}', 'UserController@update');
+    Route::delete('/user/{id}', 'UserController@destroy')->middleware('auth.jwt', 'check.role:3');
 });
